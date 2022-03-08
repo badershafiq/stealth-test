9bce4bfb89f11cb17e5c8d92fb2e35e78c2ceac2
Badar Shafiq <badarjanjua92@gmail.com>
2022-03-09 02:47:08 +0500

    Designed fill in the blank and options components

---
+++ src/components/FillBlank.tsx
@@ -0,0 +1,41 @@
+import React, { FC } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import COLORS from "../constants/colors";
+
+const FillBlank: FC<{
+  question: string;
+  option: string;
+}> = ({ question, option }) => (
+  <View style={styles.container}>
+    {question?.split(" ").map((word: String, index: number) => (
+      <View key={index}>
+        {word == "?" ? (
+          <Text style={styles.selected}>{option ? option : "____"}</Text>
+        ) : (
+          <Text style={styles.question}> {word} </Text>
+        )}
+      </View>
+    ))}
+  </View>
+);
+
+export default FillBlank;
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  question: {
+    color: COLORS.primary,
+    fontFamily: "Cochin",
+    fontSize: 22,
+  },
+  selected: {
+    color: COLORS.primary,
+    fontFamily: "Cochin",
+    fontSize: 22,
+    fontWeight: "700",
+    textDecorationLine: "underline",
+  },
+});
---
+++ src/components/Option.tsx
@@ -0,0 +1,49 @@
+import React, { FC } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import COLORS from "../constants/colors";
+
+const Option: FC<{
+  text: string;
+  selected: string;
+  setSelected: React.Dispatch<React.SetStateAction<string>>;
+}> = ({ text, selected, setSelected }) => (
+  <View
+    style={[
+      styles.optionContainer,
+      selected === text && styles.selectedContainer,
+    ]}
+  >
+    <TouchableOpacity onPress={() => setSelected(text)}>
+      {selected === text ? (
+        <Text style={[styles.option, styles.selected]}>{text}</Text>
+      ) : (
+        <Text style={styles.option}>{text}</Text>
+      )}
+    </TouchableOpacity>
+  </View>
+);
+
+export default Option;
+
+const styles = StyleSheet.create({
+  optionContainer: {
+    margin: "3%",
+    width: "35%",
+    borderRadius: 10,
+    backgroundColor: COLORS.primary,
+  },
+  selectedContainer: {
+    backgroundColor: COLORS.optionSelected,
+  },
+  option: {
+    color: COLORS.background,
+    padding: "10%",
+    fontFamily: "Cochin",
+    fontSize: 16,
+    fontWeight: "700",
+    textAlign: "center",
+  },
+  selected: {
+    color: COLORS.primary,
+  },
+});
