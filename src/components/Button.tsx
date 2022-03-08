2e23530b7e7a5514945a07be2d435ab9dfe2bfc9
Badar Shafiq <badarjanjua92@gmail.com>
2022-03-09 02:44:50 +0500

    Designed bottom button

---
+++ src/components/Button.tsx
@@ -0,0 +1,40 @@
+import React, { FC } from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import COLORS from "../constants/colors";
+
+const Button: FC<{
+  selected: string;
+  completed: boolean;
+  submit: () => void;
+  restart: () => void;
+}> = ({ selected, completed, submit, restart }) => (
+  <TouchableOpacity
+    style={styles.continueContainer}
+    onPress={completed ? restart : submit}
+  >
+    <Text style={styles.continueText}>
+      {completed ? "Start Again" : selected ? "Check Answer" : "Continue"}
+    </Text>
+  </TouchableOpacity>
+);
+
+export default Button;
+
+const styles = StyleSheet.create({
+  continueContainer: {
+    alignItems: "center",
+    backgroundColor: COLORS.optionSelected,
+    borderRadius: 10,
+    marginTop: "auto",
+    marginBottom: "10%",
+    paddingHorizontal: "10%",
+    paddingVertical: "5%",
+    width: "60%",
+  },
+  continueText: {
+    color: COLORS.primary,
+    fontFamily: "Cochin",
+    fontSize: 22,
+    fontWeight: "700",
+  },
+});
